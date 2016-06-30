b363d1809b8d976696d068457340ad6a0844e5ff
RKathees <kathees@wso2.com>
2016-06-30 09:48:06 +0530

    Update RecurlyConnectorIntegrationTest.java

--- src/test/java/org/wso2/carbon/connector/integration/test/recurly/RecurlyConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/recurly/RecurlyConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class RecurlyConnectorIntegrationTest extends ConnectorIntegrationTestBas
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("recurly-connector-1.0.1");
+        init("recurly-connector-1.0.2-SNAPSHOT");
 
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/xml");
